e428f4e774ab76aba4e2b9c825fa35035eac215e
Izuwei <37940354+Izuwei@users.noreply.github.com>
2019-10-25 15:00:37 +0200

    Add files via upload

---
+++ precache-manifest.27cb14daff5fb8d3b7c6b609481591bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a16b731a6f6266bd46c797976995c5",
+    "url": "/production/index.html"
+  },
+  {
+    "revision": "f03a18f96703d511e92c",
+    "url": "/production/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "51e7a943e5e8bd3e6d53",
+    "url": "/production/static/js/2.7bbcee2d.chunk.js"
+  },
+  {
+    "revision": "f03a18f96703d511e92c",
+    "url": "/production/static/js/main.a5e1d32c.chunk.js"
+  },
+  {
+    "revision": "3f01327773520a3bc270",
+    "url": "/production/static/js/runtime-main.98282ae4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/production/static/media/logo.25bf045c.svg"
+  }
+]);
