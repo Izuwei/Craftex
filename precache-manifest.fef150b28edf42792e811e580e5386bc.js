677e9a6578aef3b045172b7b2d4999418d2c6ee7
Izuwei <sadilekjames@gmail.com>
2019-10-25 14:36:10 +0200

    Updates

---
+++ precache-manifest.fef150b28edf42792e811e580e5386bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3299bb2d8f510dba00a6316fb8c17962",
+    "url": "/production/index.html"
+  },
+  {
+    "revision": "c3ffaa3615774e9cc680",
+    "url": "/production/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "51e7a943e5e8bd3e6d53",
+    "url": "/production/static/js/2.7bbcee2d.chunk.js"
+  },
+  {
+    "revision": "c3ffaa3615774e9cc680",
+    "url": "/production/static/js/main.4d877a55.chunk.js"
+  },
+  {
+    "revision": "3f01327773520a3bc270",
+    "url": "/production/static/js/runtime-main.98282ae4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/production/static/media/logo.25bf045c.svg"
+  }
+]);
