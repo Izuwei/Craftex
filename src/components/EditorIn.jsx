559ba010c55eaad1c58e4e33461e3ba7d8135f48
Izuwei <sadilekjames@gmail.com>
2020-02-06 17:22:29 +0100

    pridany breakpointy, zatim bez funkcionality

--- src/components/EditorIn.jsx
+++ src/components/EditorIn.jsx
@@ -14,6 +14,43 @@ class EditorIn extends Component {
     this.onChange = this.onChange.bind(this);
     this.resize = this.resize.bind(this);
     this.onSelectionChange = this.onSelectionChange.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
+  /**
+   * https://ourcodeworld.com/articles/read/1052/how-to-add-toggle-breakpoints-on-the-ace-editor-gutter
+  */
+  componentDidMount(){
+    this.refs.aceIn.editor.on("guttermousedown", function(e) {
+      var target = e.domEvent.target;
+
+      if (target.className.indexOf("ace_gutter-cell") === -1) {
+          return;
+      }
+
+      /*// Pokud neni editor focusnut ignorovat nastaveni breakpointu (mozna se bude hodit).
+      if (!(e.editor.isFocused())){
+          return; 
+      }*/
+
+      // Misto za cislici ignorujeme pro nastaveni breakpointu
+      if (e.clientX > 25 + target.getBoundingClientRect().left) {
+          return;
+      }
+
+      var row = e.getDocumentPosition().row;
+      var breakpoints = e.editor.session.getBreakpoints(row, 0);
+
+      if (typeof breakpoints[row] === typeof undefined) {
+        e.editor.session.setBreakpoint(row);
+      }
+      else {
+          e.editor.session.clearBreakpoint(row);
+      }
+
+      e.stop();
+      console.log(breakpoints);
+    });
   }
 
   resize() {
@@ -22,6 +59,7 @@ class EditorIn extends Component {
 
   onChange(newValue) {
     this.props.edit(newValue);
+    console.log(this.refs.aceIn.editor.session.getBreakpoints());
   }
   
   onSelectionChange(newValue, event) {
@@ -36,7 +74,7 @@ class EditorIn extends Component {
         theme="idle_fingers"
         fontSize="20px"
         onChange={this.onChange}
-        onSelectionChange={this.onSelectionChange}
+        //onSelectionChange={this.onSelectionChange}
         ref="aceIn"
         value={this.props.content}
         mode="plain_text"
--- src/components/SplitEditor.jsx
+++ src/components/SplitEditor.jsx
@@ -1,30 +1,24 @@
-import React, { Component } from 'react';
+import React, { useRef } from 'react';
 import SplitPane from 'react-split-pane';
 import EditorIn from './EditorIn';
 import EditorOut from './EditorOut';
 import './SplitEditor.css';
 
-class SplitEditor extends Component {
-    state = {  }
+function SplitEditor(props) {
+    const aceIn = useRef();
+    const aceOut = useRef();
 
-    constructor(props) {
-        super(props);
-        this.handleResize = this.handleResize.bind(this);
+    const handleResize = () => {
+        aceIn.current.resize();
+        aceOut.current.resize();
     }
 
-    handleResize(){
-        this.aceIn.resize();
-        this.aceOut.resize();
-    }
-
-    render() { 
-        return ( 
-            <SplitPane className="SplitEditor" split="vertical" style={{height: "700px", position: "static"}} minSize={200} maxSize={-200} defaultSize={"50%"} onChange={ () => this.handleResize() }>
-                <EditorIn ref={ instance => { this.aceIn = instance; }} content={this.props.editorContent} edit={this.props.editText}/>
-                <EditorOut ref={ instance => { this.aceOut = instance; }} content={this.props.editorResult}/>
-            </SplitPane>
-         );
-    }
-}
+    return ( 
+        <SplitPane className="SplitEditor" split="vertical" style={{height: "700px", position: "static"}} minSize={200} maxSize={-200} defaultSize={"50%"} onChange={ () => handleResize() }>
+            <EditorIn ref={ aceIn } content={props.editorContent} edit={props.editText}/>
+            <EditorOut ref={ aceOut } content={props.editorResult}/>
+        </SplitPane>
+     );  
+};
  
 export default SplitEditor;
